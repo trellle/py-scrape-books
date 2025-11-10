b09f60d20702edc69f1c1941da0e775e9a6d734e
yarik <super.racing@ukr.net>
2025-11-10 02:23:24 +0200

    added pagination and file writing

--- books/spiders/books.py
+++ books/spiders/books.py
@@ -9,24 +9,36 @@ class BooksSpider(scrapy.Spider):
     allowed_domains = ["books.toscrape.com"]
     start_urls = ["https://books.toscrape.com"]
 
+    def open_spider(self, spider):
+        self.file = open("books.jl", "w")
+    
+    def close_spider(self, spider):
+        self.file.close()
+
     def parse_detail_page(self, response: Response):
-        yield {
-                "title": response.css(".product_main h1").get(),
-                "price": float(response.css(".product_main .price_color").get().replace("£", "")),
-                "amount_in_stock": int(response.css(".instock::text").getall()[-1].split(" ")[2].replace("(", "")),
-                "rating": w2n.word_to_num(
-                    response.css(".star-rating").attrib["class"].split(" ")[1]
-                ),
-                "category": response.css(".breadcrumb li a::text").getall()[1],
-                "description": response.css(".product-description + p::text"),
-                "upc": response.css(".table tr:first-child th::text").getall()[1]
-            }
+        data =  {
+            "title": response.css(".product_main h1").get(),
+            "price": float(response.css(".product_main .price_color").get().replace("£", "")),
+            "amount_in_stock": int(response.css(".instock::text").getall()[-1].split(" ")[2].replace("(", "")),
+            "rating": w2n.word_to_num(
+                response.css(".star-rating").attrib["class"].split(" ")[1]
+            ),
+            "category": response.css(".breadcrumb li a::text").getall()[1],
+            "description": response.css(".product-description + p::text"),
+            "upc": response.css(".table tr:first-child th::text").getall()[1]
+        }
+        self.file.write(data)
+        yield data
 
     def parse(self, response: Response):
         for book in response.css(".product_pod"):
             details_url = book.css("h3 a::attr(href)").get()
             full_url = response.urljoin(details_url)
             yield scrapy.Request(full_url, callback=self.parse_detail_page)
+        next_page = response.css(".next a::attr(href)").get()
+        if next_page:
+            url = response.urljoin(next_page)
+            yield scrapy.Request(url, callback=self.parse)
         filename = "books.jl"
         Path(filename).write_bytes(response.body)
         self.log(f"Saved file {filename}")
